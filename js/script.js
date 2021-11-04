5b2e04763ae05cf130cdb7018ff6bc9a166712ca
Maurizio <proiettomaurizio97@gmail.com>
2021-11-04 14:59:19 +0100

    first part of the complete exercise. update comments

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 //DONE=>creo un array con all'interno degli oggetti 
-// creo una funzione che generi gli elementi del DOM
-    // mi ricavo gli oggetti dell'array uno per uno, con le relative proprietà => FOR 
-    // mi ricavo tutti i valori delle chiavi dell'oggetto => FOR IN 
-    // nella funzione inserisco tutte le chiavi degli elementi 
+// DONE=>creo una funzione che generi gli elementi del DOM
+    // DONE=>scorro gli oggetti dell'array=> FOR 
+    // DONE=>per ogni oggetto ricavo i valori delle chiavi e le salvo dentro delle variabili => FOR IN 
+    // DONE=>nella funzione inserisco tutte le chiavi degli elementi 
 // creo un ascoltatore per il bottone
     // al click mi ricavo i valori inseriti
     // creo un oggetto 
@@ -15,47 +15,77 @@
 
 const teamMembers = [
     {
-        "nome" : "Wayne Barnett",
-        "immagine" : "img/wayne-barnett-founder-ceo.jpg",
-        "ruolo" : "Founder & CEO"
+        "nameTeam" : "Wayne Barnett",
+        "image" : "img/wayne-barnett-founder-ceo.jpg",
+        "role" : "Founder & CEO"
     },
     {
-        "nome" : "Angela Caroll",
-        "immagine" : "img/angela-caroll-chief-editor.jpg",
-        "ruolo" : "Chief Editor"
+        "nameTeam" : "Angela Caroll",
+        "image" : "img/angela-caroll-chief-editor.jpg",
+        "role" : "Chief Editor"
     },
     {
-        "nome" : "Walter Gordon",
-        "immagine" : "img/walter-gordon-office-manager.jpg",
-        "ruolo" : "Office Manager"
+        "nameTeam" : "Walter Gordon",
+        "image" : "img/walter-gordon-office-manager.jpg",
+        "role" : "Office Manager"
     },
     {
-        "nome" : "Angela Lopez",
-        "immagine" : "img/angela-lopez-social-media-manager.jpg",
-        "ruolo" : "Social Media Manager"
+        "nameTeam" : "Angela Lopez",
+        "image" : "img/angela-lopez-social-media-manager.jpg",
+        "role" : "Social Media Manager"
     },
     {
-        "nome" : "Scott Estrada",
-        "immagine" : "img/scott-estrada-developer.jpg",
-        "ruolo" : "developer"
+        "nameTeam" : "Scott Estrada",
+        "image" : "img/scott-estrada-developer.jpg",
+        "role" : "developer"
     },
     {
-        "nome" : "Barbara Ramos",
-        "immagine" : "img/barbara-ramos-graphic-designer.jpg",
-        "ruolo" : "Grafhic Designer"
+        "nameTeam" : "Barbara Ramos",
+        "image" : "img/barbara-ramos-graphic-designer.jpg",
+        "role" : "Grafhic Designer"
     }
 
 ];
 
-console.log(teamMembers);
+//costante per collecare il container
+const containerCard = document.querySelector(".team-container");
+//ciclo che uso per scorrere ogni elemento dell'array
 for (let i = 0; i < teamMembers.length; i++){
-    for ( let key in teamMembers[i] ){
-        console.log(key, teamMembers[i][key]);
-    }
+    // creo delle variabili per salvarmi le informazioni chiave
+    let nameTeam = teamMembers[i].nameTeam;
+    let image = teamMembers[i].image;
+    let role = teamMembers[i].role;
+    // funzione che utilizzo per stampare le card
+    createTeamCard(nameTeam, image, role );
 }
 
-
 // funzioni utili
-function name(params) {
-    
+/* function createTeamCard() {
+    let teamCard = document.createElement("div");
+    teamCard.classList.add("team-card");
+
+    let cardImage = document.createElement("div");
+    cardImage.classList.add("card-image");
+
+    let imgProfile = document.createElement("img");
+    cardImage.append(imgProfile);
+    cardImage.classList.add("card-image");
+} */
+
+
+function createTeamCard(nameTeam, image, role) {
+    containerCard.innerHTML += `
+        <div class="team-card">
+            <div class="card-image">
+              <img
+                src= ${image}
+                alt= ${nameTeam}
+              />
+            </div>
+            <div class="card-text">
+              <h3>${nameTeam}</h3>
+              <p>${role}</p>
+            </div>
+        </div>
+    `
 }
